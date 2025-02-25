c7fbe02f4e3d788dd827df7f1d8f23e2104e1524
Christopher Angelo <angelo@angeloanan.xyz>
2025-02-26 06:38:26 +0700

    feat(port-selector): common ports block & docker note

--- src/components/PortSelector.tsx
+++ src/components/PortSelector.tsx
@@ -5,12 +5,36 @@ import { ArrowPathIcon } from './icons/ArrowPath.tsx'
 
 const MINIMUM_PORT = 1024
 const MAXIMUM_PORT = 65535
+const COMMON_PORT = [
+  5432, // Postgres
+  3306, // MySQL
+  6379, // Redis
+  27017, // Mongo (mongod & mongos)
+
+  // Utils
+  1194, // OpenVPN
+  4321, // Dev HTTP
+  8008, // Dev HTTP
+  8080, // Dev HTTP
+  3000, // Dev HTTP
+  5000, // Dev HTTP
+  8000, // Dev HTTP
+  8384, // Syncthing Web GUI
+  22000, // Syncthing Protocol
+  33434, // traceroute
+
+  // Games
+  25565, // Minecraft
+  19132, // Minecraft Bedrock IPv4
+  19133 // Minecraft Bedrock IPv6
+]
 
 const selectRandomPort = (from = MINIMUM_PORT, to = MAXIMUM_PORT) => {
   return Math.floor(Math.random() * (to - from + 1) + from)
 }
 
 export const PortSelector = () => {
+  const [isExcludeCommonPort, setExcludeCommonPort] = createSignal<boolean>(false)
   const [port, setPort] = createSignal<number>()
 
   const copyPort = () => {
@@ -18,7 +42,12 @@ export const PortSelector = () => {
   }
 
   const reroll = () => {
-    setPort(selectRandomPort())
+    setPort(() => {
+      let port = selectRandomPort()
+      // Imagine if you're *that* unlucky
+      while (isExcludeCommonPort() && COMMON_PORT.includes(port)) port = selectRandomPort()
+      return port
+    })
   }
 
   onMount(reroll)
@@ -32,7 +61,7 @@ export const PortSelector = () => {
 
       <div class='mx-auto mt-2 flex justify-center'>
         <button
-          class='mx-2 flex items-center rounded-full bg-sky-600 px-4 py-2 text-sky-100 active:bg-sky-700'
+          class='mx-2 flex items-center rounded-full bg-sky-600 px-4 py-2 text-sky-100 hover:bg-sky-500 active:bg-sky-700'
           onClick={copyPort}
           disabled={port() == null}
         >
@@ -40,13 +69,35 @@ export const PortSelector = () => {
           <span class='ml-1'>Copy</span>
         </button>
         <button
-          class='mx-2 flex items-center rounded-full bg-red-600 px-4 py-2 text-red-100'
+          class='mx-2 flex items-center rounded-full bg-red-600 px-4 py-2 text-red-100 hover:bg-red-500 active:bg-red-700'
           onClick={reroll}
         >
           <ArrowPathIcon />
           <span class='ml-1'>Re-roll</span>
         </button>
       </div>
+
+      <section class='mt-2'>
+        <div class='block'>
+          <input
+            type='checkbox'
+            id='toggleCommon'
+            checked={isExcludeCommonPort()}
+            onToggle={() => {
+              setExcludeCommonPort((p) => !p)
+              if (COMMON_PORT.includes(port() ?? 0)) reroll()
+            }}
+          />
+          <label for='toggleCommon' class='ml-2'>
+            Exclude common port
+          </label>
+        </div>
+
+        <hr class='mx-auto my-4 max-w-48'></hr>
+
+        <div class='mt-4 font-light'>Docker port mapping:</div>
+        <div class='font-mono'>outside:inside</div>
+      </section>
     </div>
   )
 }
